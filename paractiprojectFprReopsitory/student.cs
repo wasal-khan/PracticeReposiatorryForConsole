91936545da4bb25bd0393daa683180d5a8cd6a8e
wasal-khan <wisalkhan592@gmail.com>
2025-10-04 00:03:50 -0700

    yes

--- paractiprojectFprReopsitory/student.cs
+++ paractiprojectFprReopsitory/student.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using paractiprojectFprReopsitory;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,5 +13,32 @@ namespace paractiprojectFprReopsitory
         public int Name { get; set; }
         public int Id { get; set; }
         public int Age { get; set; }
+        
+    }
+
+   
+}
+public class StudentRepository
+{
+    private List<Student> students = new List<Student>();
+    public void AddStudent(Student student)
+    {
+        students.Add(student);
+    }
+    public void RemoveStudent(int id)
+    {
+        var student = students.FirstOrDefault(s => s.Id == id);
+        if (student != null)
+        {
+            students.Remove(student);
+        }
+    }
+    public Student GetStudent(int id)
+    {
+        return students.FirstOrDefault(s => s.Id == id);
+    }
+    public List<Student> GetAllStudents()
+    {
+        return students;
     }
 }
