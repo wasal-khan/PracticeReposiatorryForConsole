a0f23e991abe57d16b98fea49785c85365f1e325
wasal-khan <wisalkhan592@gmail.com>
2025-10-03 23:54:33 -0700

    this is the student classe wich i have created to manage the student record and mor

---
+++ paractiprojectFprReopsitory/student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace paractiprojectFprReopsitory
+{
+    public class Student
+    {
+        public int Name { get; set; }
+        public int Id { get; set; }
+    }
+}
