5983c68cc0dcb9bca1a0f2a5a6a3b6f4683304af
wasal-khan <wisalkhan592@gmail.com>
2025-10-04 00:11:34 -0700

    deleated there methods on this class

--- paractiprojectFprReopsitory/student.cs
+++ paractiprojectFprReopsitory/student.cs
@@ -25,25 +25,5 @@ public class StudentRepository
     {
         students.Add(student);
     }
-    public void RemoveStudent(int id)
-    {
-        var student = students.FirstOrDefault(s => s.Id == id);
-        if (student != null)
-        {
-            students.Remove(student);
-        }
-    }
-    public Student GetStudent(int id)
-    {
-        return students.FirstOrDefault(s => s.Id == id);
-    }
-    public List<Student> GetAllStudents()
-    {
-        Console.WriteLine("This is a test repository for Paracti Project");
-        return students;
-    }
-    public List<Student> GetAllStudentss()
-    {
-        return students;
-    }
+ 
 }
